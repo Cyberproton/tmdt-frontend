d925787a584d6a079524694722df77763980cd10
ducwnn <nhduc9501@gmail.com>
2023-04-12 22:30:13 +0700

    cart-page-edit & add checkout-page

--- src/app/pages/CartPage/pages/Cart.tsx
+++ src/app/pages/CartPage/pages/Cart.tsx
@@ -1,17 +1,27 @@
 import { Col, Row } from 'react-bootstrap';
 import { StoreItem } from '../components/StoreItem';
 import storeItems from '../data/items.json';
-//import { useShoppingCart } from '../context/ShoppingCartContext';
-//import MetaData from './MetaData';
+import { ShoppingCartProvider } from '../context/ShoppingCartContext';
+import { DesignBreadcrumbs } from '../components/DesignBreadcrumb';
+import { Helmet } from 'react-helmet-async';
 
 export function MyCart() {
   return (
     <>
-      <h1>Cart</h1>
+    <Helmet>
+        <title>Thông tin</title>
+        <meta
+          name="description"
+          content="A React Boilerplate application homepage"
+        />
+    </Helmet>
+      <h1>Giỏ hàng</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
-            <StoreItem {...item} />
+            <ShoppingCartProvider>
+              <StoreItem {...item} />
+            </ShoppingCartProvider>
           </Col>
         ))}
       </Row>
